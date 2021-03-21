de001112a3b396919b1da2f0a6053056c2356419
Jordan Russel <rudzinski.a@gmail.com>
2021-03-21 23:51:12 +0100

    adding htmlnanorc file to prevent minifying svgs

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@
 import 'regenerator-runtime/runtime'
 import PortraitAnimations from './scripts/animatePortrait';
 import correctVertical from './scripts/correctVertical';
-// import mobileAndTabletCheck from './scripts/checkTouchScreen';
+import mobileAndTabletCheck from './scripts/checkTouchScreen';
 import './scss/global.scss';
 import './scss/pages/index.scss';
 
 const app = () => {
-    // PortraitAnimations();
-    // correctVertical();
-    // console.log(window.mobileAndTabletCheck())
-    // if(window.mobileAndTabletCheck()) {
-    //     document.getElementById("about-link").style.color = "green";
-    // }
+    PortraitAnimations();
+    correctVertical();
+    console.log(window.mobileAndTabletCheck())
+    if(window.mobileAndTabletCheck()) {
+        document.getElementById("about-link").style.color = "green";
+    }
 }
 
 app();
